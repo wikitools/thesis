5d84a37fa0e21b9fb4515b2eddce0d6d7258f803
Stanisław Góra <stasgora@gmail.com>
2019-11-30 17:05:13 +0100

    update

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -33,6 +33,7 @@ tlmgr option -- autobackup 0
 # Update the TL install but add nothing new
 tlmgr update --self --all --no-auto-install
 
+sudo apt-get update
 sudo apt-get install ttf-mscorefonts-installer
 sudo fc-cache
 fc-match Arial
